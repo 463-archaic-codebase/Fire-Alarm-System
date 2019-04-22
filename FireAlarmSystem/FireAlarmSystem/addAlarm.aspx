113374f7761cfb213f5c9b5ad9a8a87f5f83ae39
alex-marquette <35579617+alex-marquette@users.noreply.github.com>
2019-04-21 20:58:28 -0500

    Add/Delete Pages add and working...not my best stuff

---
+++ FireAlarmSystem/FireAlarmSystem/addAlarm.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="addAlarm.aspx.cs" Inherits="FireAlarmSystem.addAlarm" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        <br />
+    </div>
+
+    <div class="row ">
+        <div class="col-lg-6 col-lg-offset-3">
+            <div class="panel">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h2> Add a New Alarm to the system</h2>
+                        
+                        <p>The following form allows you to enter a new alarm to the database</p>
+                    </div>
+                    
+                </div>
+                <br />
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="col-form-label align-right">
+                            Alarm Type
+                        </div>
+                    </div>
+                    <div class="col-lg-7" id="alarmType">
+                         <%-- AutoCompleteType from dB --%>
+                            <asp:DropDownList ID="ddlAlarmType" runat="server" CssClass="form-control aspnet-width-fix" OnSelectedIndexChanged="ddlAlarmType_SelectedIndexChanged">
+                           
+                        </asp:DropDownList>
+                    </div>
+                </div>
+                <br />
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="col-form-label align-right">
+                            Alarm Location
+                        </div>
+                    </div>
+                    <div class="col-lg-7" id="alarmLocation">
+                      <asp:DropDownList ID="ddlLocation" runat="server" CssClass="form-control aspnet-width-fix" OnSelectedIndexChanged="ddlAlarmLocation_SelectedIndexChanged">
+                          <%--filled by db--%>
+                      </asp:DropDownList>   
+                    </div>
+                </div>
+                <br />
+                 <div class="row">
+                    <div class="col-lg-3">
+                        <div class="col-form-label align-right">
+                            Alarm Description
+                        </div>
+                    </div>
+                    <div class="col-lg-7" id="alarmDescriptionField">
+                      <asp:TextBox id="alarmDescription" runat="server" MaxLength="50" rows="5" Columns="50" Enabled="true" ForeColor="Black" TextMode="MultiLine"></asp:TextBox>   
+                    </div>
+                </div>
+                <br />
+                
+                <div class="row">
+                    <div class="col-lg-4">
+                        <asp:Button runat="server" ID="insertBtn" Text="Add alarm" CssClass="btn-success btn btn-lg btn-block aspnet-width-fix" OnClick="insertBtn_Click" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <asp:Label ID="lblError" runat="server" Visible="false"></asp:Label>
+    <asp:Label ID="lblMsg" runat="server" Visible="false"></asp:Label>
+</asp:Content>
---
+++ FireAlarmSystem/FireAlarmSystem/addAlarm.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace FireAlarmSystem {
+    public partial class addAlarm : System.Web.UI.Page {
+        private string cs = ConfigurationManager.ConnectionStrings["CString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e) {
+            if(!IsPostBack) {
+                fillLocationDDL();
+                fillAlarmTypeDDL();
+            }
+                
+        }
+
+        private void fillAlarmTypeDDL() {
+            try {
+                using (MySqlConnection conn = new MySqlConnection(cs)) {
+                    var procedure = "USP_Select_AlarmType";
+                    MySqlCommand cmd = new MySqlCommand(procedure, conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (MySqlDataAdapter da = new MySqlDataAdapter(cmd)) {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        ddlAlarmType.DataSource = dt;
+                        ddlAlarmType.DataTextField = "alarmType";
+                        ddlAlarmType.DataValueField = "alarmTypeID";
+                        ddlAlarmType.DataBind();
+                        ddlAlarmType.SelectedIndex = 0;
+                    }
+                }
+            } catch (MySqlException ex) {
+                lblError.Text = "Could not retrieve alarm type: " + ex.ToString();
+                lblError.Visible = true;
+            }
+        }
+
+        protected void ddlAlarmLocation_SelectedIndexChanged(object sender, EventArgs e) {
+            fillLocationDDL();
+        }
+
+        private void fillLocationDDL() {
+            try {
+                using (MySqlConnection conn = new MySqlConnection(cs)) {
+                    var procedure = "USP_Select_Locations";
+                    MySqlCommand cmd = new MySqlCommand(procedure, conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (MySqlDataAdapter da = new MySqlDataAdapter(cmd)) {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        ddlLocation.DataSource = dt;
+                        ddlLocation.DataTextField = "locationDescription";
+                        ddlLocation.DataValueField = "locationID";
+                        ddlLocation.DataBind();
+                        ddlLocation.SelectedIndex = 0;
+                    }
+                }
+            } catch (MySqlException ex) {
+                lblError.Text = "Could not retrieve available locations: " + ex.ToString();
+                lblError.Visible = true;
+            }
+        }
+
+        
+
+       
+
+        protected void ddlAlarmType_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        protected void insertBtn_Click(object sender, EventArgs e) {
+            try {
+                using (MySqlConnection conn = new MySqlConnection(cs)) {
+                    
+
+                    var procedure = "USP_Insert_New_Alarm";
+                    MySqlCommand cmd = new MySqlCommand(procedure, conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("alarmTypeID", ddlAlarmType.SelectedValue);
+                    cmd.Parameters.AddWithValue("description", alarmDescription.Text);
+                    cmd.Parameters.AddWithValue("locationID", ddlLocation.SelectedValue);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    lblMsg.Text = "Alarm Succesfully Added";
+                    lblMsg.Visible = true;
+                }
+            } catch(MySqlException ex) {
+                lblError.Text = "Unable to add new alarm to DB: " + ex.ToString();
+                lblError.Visible = true;
+            }
+        }
+
+       
+    }
+}
